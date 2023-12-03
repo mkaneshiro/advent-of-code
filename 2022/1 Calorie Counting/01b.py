e9e1985fdf30f0c630862aa3b005ab783bce41f4
mkaneshiro <malinakaneshiro@gmail.com>
2023-12-02 22:50:19 -0500

    Added 2022 puzzle folder

---
+++ 2022/1 Calorie Counting/01b.py
@@ -0,0 +1,25 @@
+# mkaneshiro
+# 2023-12-02
+# Advent of Code 2022
+# Day 1: Calorie Counting, Part 2
+
+with open("input.txt", "r") as f:
+    inps = f.read().splitlines()
+    f.close()
+
+splinps = []
+temp = []
+
+for line in inps:
+    if line == "":
+        splinps.append(temp)
+        temp = []
+    else:
+        temp.append(int(line))
+
+def solve(elves):
+    return sum(sorted([sum(elf) for elf in elves])[-3:])
+
+print(solve(splinps))
+
+# Answer: 212836
